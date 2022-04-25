f33e5bab2bdb8fd44bf1782a3b6437a97854bcca
Michael J <johnson_michael1329@yahoo.com>
2022-04-25 16:46:22 -0500

    Add POSTMAN files + update models.py file to have the user input the date field.

--- music_library/models.py
+++ music_library/models.py
@@ -5,5 +5,5 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField(auto_now_add=True)
+    release_date = models.DateField()
     genre = models.CharField(max_length=255)
