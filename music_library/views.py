d5541ff051a4c715155a3ddf562b3c85b1f90350
Michael J <johnson_michael1329@yahoo.com>
2022-04-25 14:50:56 -0500

    Update music_library urls.py + Create GET, POST, PUT, and DELETE methods in views.py file.

--- music_library/views.py
+++ music_library/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
 # Create your views here.
-@api_view(['GET'])
-def songs_list(request):
+@api_view(['GET', 'POST'])
+def song_list(request):
+  song = Song.objects.all()
+  if request.method == 'GET':
+    serializer = SongSerializer(song, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = SongSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response('ok')
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
